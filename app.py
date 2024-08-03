db77bbb1ea5a7985a09fdc5c3e6bdbeebb1a99e4
PrathamChauhan <33632299+Pratham-Chauhan@users.noreply.github.com>
2024-08-03 17:42:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def get_prices():
         data.append({'style_id': style_id, 'mrp': mrp, 'price': price})
     return jsonify(data)
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run('0.0.0.0', port=5000)
